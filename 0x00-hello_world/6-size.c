026330d6f244013a5b84f3189fd90d669019eb7e
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-03 23:11:57 +0000

    try6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("Size of an char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of an long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of an long long: %lu byte(s)\n", sizeof(long long));
+printf("Size of an float: %lu byte(s)\n", sizeof(float));
+return (0);
 }
