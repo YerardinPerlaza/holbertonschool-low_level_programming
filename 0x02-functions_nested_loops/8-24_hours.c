af1f46ce2c17463d5daf985e563892c12f288abd
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-09 16:48:00 +0000

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,14 @@
 #include "holberton.h"
-
 /**
- * main - check the code for Holberton School students.
+ * jack_bauer - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-
 void jack_bauer(void)
 {
 	int hour;
 	int minute;
+
 	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
