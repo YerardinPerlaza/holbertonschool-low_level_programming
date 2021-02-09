58567b747462e485c5686096492923b5a359fa59
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-09 16:50:18 +0000

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
+ * add - check the code for Holberton School students.
+ *
+ *@a: int
+ *
+ *@b: int
  *
  * Return: Always 0.
  */
 int add(int a, int b)
 {
 	int suma;
+
 	suma = (a + b);
 	return (suma);
 }
