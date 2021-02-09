0d1016008770d762ef407ae5025bd2250df1e778
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-09 16:26:32 +0000

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * main - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -8,6 +8,7 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i;
+
 	for (i = 0; i < 10; ++i)
 	{
 		for (c = 'a'; c <= 'z'; c++)
