4a465ca105ea4435635c386752d925a0237113d5
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-09 22:31:09 +0000

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,41 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * times_table - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 void times_table(void)
 {
+	int fila;
+	int columna;
+	int mult;
 
+	for (fila = 0; fila <= 9; fila++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (columna = 1; columna <= 9; columna++)
+		{
 
+			mult = (fila * columna);
+			if ((mult / 10) > 0)
+			{
+				_putchar((mult / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((mult % 10) + '0');
+
+			if (columna < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
