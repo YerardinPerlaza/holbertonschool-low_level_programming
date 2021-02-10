ec799f5985f259f00160ddf9f711acb6b0fe4237
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-10 02:17:11 +0000

    100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - check the code for Holberton School students.
+ *
+ *@n: int
+ *
+ * Return: Always 0.
+ */
+void print_times_table(int n)
+{
+	int fila, columna, mult;
+
+	if (n > 0 && n < 15)
+	{
+		for (fila = 0; fila <= n; fila++)
+		{
+			_putchar('0');
+			_putchar(',');
+			_putchar(' ');
+			for (columna = 1; columna <= n; columna++)
+			{
+				mult = (fila * columna);
+				if ((mult / 100) > 0)
+				{
+					_putchar((mult / 100) + '0');
+					_putchar(((mult / 10) % 10) + '0');
+					_putchar((mult % 10) + '0');
+				}
+				else if ((mult / 10) > 0)
+				{
+					_putchar(' ');
+					_putchar((mult / 10) + '0');
+					_putchar((mult % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((mult % 10) + '0');
+				}
+				if (columna < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
