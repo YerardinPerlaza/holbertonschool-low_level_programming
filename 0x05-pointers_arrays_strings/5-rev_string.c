d5eef033125ff6898654a916edfa1afe2d8bce02
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-16 20:08:53 +0000

    solvenew5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,11 +15,14 @@ void rev_string(char *s)
 
 	for (n = 0; s[n] != 0; n++)
 	{
-		for (i = 0; i < n / 2; i++)
-		{
-			temp = s[i];
-			s[i] = s[n - 1 - i];
-			s[n - 1 - i] = temp;
-		}
+	}
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[n - 1 - i];
+		s[n - 1 - i] = temp;
+	}
+	for (i = 0; i < n; i++)
+	{
 	}
 }
