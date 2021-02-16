85873d112741960f4d36f2fc60f1b65a6b4620c3
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-16 22:38:32 +0000

    6sincontadorde2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,11 @@
  */
 void puts2(char *str)
 {
-	int i;
-
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		if ((*str % 2) == 0)
+			_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
