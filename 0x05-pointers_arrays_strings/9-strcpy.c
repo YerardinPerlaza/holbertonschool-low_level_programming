ee37ae5e152553522d27c30927c3c2a3db211fc2
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-17 01:16:19 +0000

    solve9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,17 +2,27 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * *_strcpy - check the code for Holberton School students.
+ *
+ *@dest: char
+ *@src: char
  *
  * Return: Always 0.
  */
 char *_strcpy(char *dest, char *src)
 {
+	int i;
+
+	i = 0;
 	while (*src != '\0')
 	{
 		*dest = *src;
 		src++;
+		dest++;
+		i++;
 	}
+	*dest = '\0';
+	dest = dest - i;
 
 	return (dest);
 }
