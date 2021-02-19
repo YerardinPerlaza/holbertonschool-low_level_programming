772a744ce67d9e4078467893205f91ab7bff8ebf
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-19 03:34:54 +0000

    solve6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *cap_string - check the code for Holberton School students.
+ *
+ *@str: char
+ *
+ * Return: Always 0.
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char sal[] = {' ', '\n', '\t', ',', ';', '.', '!', '?', '"', '(', ')', '}', '{'};
+
+	for (i=0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+				str[i] = str[i] - 32;
+		}
+		for (j = 0; sal[j] != '\0'; j++)
+		{
+			if (str[i] == sal[j])
+			{
+				i++;
+				if (str[i]>='a' && str[i]<='z')
+					str[i]=str[i]-32;
+				i--;
+			}
+		}
+	}
+	return (str);
+}
