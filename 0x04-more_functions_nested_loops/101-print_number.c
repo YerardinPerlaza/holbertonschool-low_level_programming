c141d26389d5c77ac93e0375c5856cafb3086e42
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-19 23:37:45 +0000

    advance

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+/**
+ * print_number - check the code for Holberton School students.
+ *
+ *@n: int
+ *
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n == 0)
+		_putchar('0');
+	else if ((n / 1000) > 0)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if ((n / 100) > 0)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if ((n / 10) > 0)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		_putchar((n % 10) + '0');
+	}
+}
