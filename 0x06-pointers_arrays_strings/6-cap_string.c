d4193d5c60f961b103cf9906eadda6f412d229c3
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-19 23:28:16 +0000

    100advance

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,8 +11,8 @@
 char *cap_string(char *str)
 {
 	int i, j;
-	char sl[] = {' ', '\n', '\t', ',', ';', '.'};
-	char sl2[] = {'!', '?', '"', '(', ')', '}', '{', '}'};
+	char sl[] = {' ', '\n', '\t', ',', ';', '.',
+		     '!', '?', '"', '(', ')', '}', '{', '}'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
@@ -31,16 +31,6 @@ char *cap_string(char *str)
 				i--;
 			}
 		}
-		for (j = 0; sl2[j] != '\0'; j++)
-		{
-			if (str[i] == sl[j])
-			{
-				i++;
-				if (str[i] >= 'a' && str[i] <= 'z')
-					str[i] = str[i] - 32;
-				i--;
-			}
-		}
 	}
-	return (str);
+		return (str);
 }
