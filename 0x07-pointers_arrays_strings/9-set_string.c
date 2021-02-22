caea27be93cf4772f3bade9a8fc522f191ba56cc
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-22 23:48:46 +0000

    solve8-9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * set_string - Entry point
+ *@s: char
+ *@to: char
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
