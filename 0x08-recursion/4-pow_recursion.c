2449c563ec69bd00fbafeb20d00582dbcf7ae484
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-24 20:00:42 +0000

    betty4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * _pow_recursion - check the code for Holberton School students.
+ *@x: int
+ *@y: int
  * Return: Always 0.
  */
 int _pow_recursion(int x, int y)
