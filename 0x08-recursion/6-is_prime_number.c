78f3228a9ffd19b4528fcb0c77e616e77285e6c4
YerardinPerlaza <yerardinpm@gmail.com>
2021-02-24 23:34:19 +0000

    solve6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,24 +1,31 @@
 #include "holberton.h"
-
+int prime_checker(int n, int i);
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * is_prime_number - executes prime_checker
+ * @n: input to check
+ * Return: Always 0 (Success)
  */
 int is_prime_number(int n)
 {
-        if (((((factorial(n - 1)) + 1) / n) % 10) == 0)
-                return (1);
-        else
-                return (0);
+	if (n <= 1)
+		return (0);
+	else if (prime_checker(n, n / 2) > 0)
+		return (1);
+	return (0);
 }
 
-int factorial(int n)
+/**
+ * prime_checker - checks for prime
+ * @n: input to check
+ * @i: n / 2, then passes to i - 1, checks if greater than 0
+ * Return: prime check
+ */
+int prime_checker(int n, int i)
 {
-        if (n < 0)
-                return (0);
-        else if (n == 0)
-                return (1);
-        else
-                return (n * factorial(n - 1));
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	else
+		return (prime_checker(n, i - 1));
 }
