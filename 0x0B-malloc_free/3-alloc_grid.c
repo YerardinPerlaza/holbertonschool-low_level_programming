f290e0005bba7614140e1d636bba98054e1becba
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-02 23:23:09 +0000

    firsttry3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	array = malloc(sizeof(int) * height);
+
+	if (array == NULL)
+	{
+		free(array);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(sizeof(int) * width);
+		if (array == NULL)
+		{
+			free(array);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+	return (array);
+
+}
