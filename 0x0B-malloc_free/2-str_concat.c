b288a4e921cfbeba2b1e128ea2b143988fc8bdaf
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-02 23:47:56 +0000

    fixstringvacion

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *array;
 	int i = 0, l1 = 0, l2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = " ";
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
 
 	while (s1[l1] != '\0')
 	{
