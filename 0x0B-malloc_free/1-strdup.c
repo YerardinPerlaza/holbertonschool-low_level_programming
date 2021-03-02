82ddf59668b81d1eaf2dc1573d28d1b2b7c5bee0
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-02 20:39:13 +0000

    solve1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - check the code for Holberton School students.
+ *@str: char
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int i, largo;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (largo = 0; largo <= *str; largo++)
+	{
+	}
+	largo += 1;
+
+	copy = malloc(sizeof(str) * largo);
+	for (i = 0; i < largo; i++)
+		copy[i] = str[i];
+
+	if (copy == NULL)
+		return (NULL);
+	return (copy);
+
+}
