d4a04a01ec31d0b6118070d76ad1036de96e9b4a
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-04 19:35:11 +0000

    solve101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,14 +9,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul = 0;
+	int mul = 0, num1, num2;
 
 	if (argc != 3)
 	{
 		printf("%s\n", "Error");
 		exit(98);
 	}
-	mul = (atoi(argv[1]) * atoi(argv[2]));
+	if (!atoi(argv[1]) || !atoi(argv[2]))
+	{
+		printf("%s\n", "Error");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	mul = (num1 * num2);
 	printf("%d\n", mul);
-	exit(98);
+	return (mul);
 }
