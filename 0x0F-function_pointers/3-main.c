cea90e8fba377429819a0c090295f7566a5169d8
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-11 01:11:53 +0000

    fixcheck24

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,7 +25,8 @@ int main(int argc, char *argv[])
 	}
 
 	operation = get_op_func(operator);
-	if (operation == NULL)
+
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
