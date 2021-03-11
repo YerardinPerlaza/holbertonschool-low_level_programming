9655a62d1d3d3c2ac669bdcd63f433cff2bb96e1
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-11 19:50:03 +0000

    solve1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,9 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	int num;
 
-	if (separator == NULL)
-		return;
-
 	va_start(ap, n);
 
 	for (i = 0; i < n; i++)
