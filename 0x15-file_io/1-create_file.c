bd002847ce93a183a37e8c73f67ec303212283ef
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-29 20:26:41 +0000

    addedgecase1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+
 	if (fd == -1)
 		return (-1);
 
@@ -23,6 +24,8 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
+	else if ((write(fd, text_content, i)) == -1)
+		return (-1);
 	else
 	{
 		write(fd, text_content, i);
