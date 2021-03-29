ca906977cc453ff01291692f708c849030fbbc71
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-29 19:53:18 +0000

    solve1and2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * create_file - Entry Point
+ * @filename: file name
+ * @text_content: null terminated string to write
+ * Return: 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+
+	while (text_content[i])
+		i++;
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	else
+	{
+		write(fd, text_content, i);
+	}
+
+	close(fd);
+	return (1);
+}
