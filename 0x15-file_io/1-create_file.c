f685279087a049c4366bb136327a2fbf318259ba
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-29 20:40:49 +0000

    newedge

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,21 +12,18 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i])
-		i++;
-
 	if (text_content == NULL)
-	{
 		text_content = "";
-	}
-	else
+
+	for (i = 0; text_content[i] != '\0'; i++)
 	{
-		write(fd, text_content, i);
+		if (write(fd, text_content, i) == -1)
+			return (-1);
 	}
 
 	close(fd);
