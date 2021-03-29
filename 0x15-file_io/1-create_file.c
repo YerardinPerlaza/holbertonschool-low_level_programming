1f1a3500e7f82d18f7ed5f2a7cf44064ca1f596a
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-29 20:32:42 +0000

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
@@ -27,8 +27,6 @@ int create_file(const char *filename, char *text_content)
 	else
 	{
 		write(fd, text_content, i);
-		if (write(fd, text_content, i) == -1)
-			return (-1);
 	}
 
 	close(fd);
