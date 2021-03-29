e1136ae142ce204f5fcb1988727dbc39133e2e94
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-29 20:16:18 +0000

    changetext

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,13 +14,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-		return (0);
-
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
 		return (0);
 
 	rd = read(fd, buf, letters);
@@ -29,7 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buf[letters] = '\0';
 
-	wr = write(1, buf, rd);
+	wr = write(STDOUT_FILENO, buf, rd);
 	if (wr == -1)
 		return (0);
 
