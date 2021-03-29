5f80eee502b2b998e9e23e24f821835598af6286
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-29 20:48:13 +0000

    again1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,7 @@ int create_file(const char *filename, char *text_content)
 		i++;
 
 	if (text_content == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
+		text_content = "";
 	else
 	{
 		write(fd, text_content, i);
