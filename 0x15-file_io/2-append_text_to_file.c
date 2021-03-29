b1f9b8349d35ea8f346705c4a84e091f9e4ecfb5
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-29 21:13:46 +0000

    solve2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		return (1);
-	else
+	if (text_content != NULL)
 	{
 		while (text_content[i] != '\0')
 			i++;
