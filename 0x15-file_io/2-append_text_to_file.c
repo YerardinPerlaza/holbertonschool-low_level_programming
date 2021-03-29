a9f154b879ea6fbbf604051c36c3bb115758f6ab
YerardinPerlaza <yerardinpm@gmail.com>
2021-03-29 21:12:26 +0000

    solve2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - Entry Point
+ * @filename: file name
+ * @text_content: text content
+ * Return: 1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+	else
+	{
+		while (text_content[i] != '\0')
+			i++;
+
+		if (write(fd, text_content, i) == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+
+}
