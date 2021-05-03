bd88dc0ab7ac452ac2374e2b85c4629707fd7906
YerardinPerlaza <yerardinpm@gmail.com>
2021-05-03 21:46:49 +0000

    advance 3

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,69 @@
+#include "calculator.h"
+
+/**
+ * add - Entry point
+ * @a: int
+ * @b: int
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int suma;
+
+	suma = a + b;
+	return (suma);
+}
+
+/**
+ * sub - Entry point
+ * @a: int
+ * @b: int
+ * Return: Always 0 (Success)
+ */
+int sub(int a, int b)
+{
+	int resta;
+
+	resta = a - b;
+	return (resta);
+}
+/**
+ * mul - Entry point
+ * @a: int
+ * @b: int
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
+/**
+ * div - Entry point
+ * @a: int
+ * @b: int
+ * Return: Always 0 (Success)
+ */
+int div(int a, int b)
+{
+	int div;
+
+	div = a / b;
+	return (div);
+}
+/**
+ * mod - Entry point
+ * @a: int
+ * @b: int
+ * Return: Always 0 (Success)
+ */
+int mod(int a, int b)
+{
+	int modulo;
+
+	modulo = a + b;
+	return (modulo);
+}
